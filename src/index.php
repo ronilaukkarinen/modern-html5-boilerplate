3f2bb64efed1f7bb5c7d8f27f390c33162a2b7b9
Roni Laukkarinen <roni@laukkarinen.info>
2015-03-04 09:26:31 +0200

    wrap header around container

--- src/index.php
+++ src/index.php
@@ -26,16 +26,22 @@
     <div id="page">
 
         <div class="header-container">
-            <header class="wrapper clearfix">
-                <h1 class="title">h1.title</h1>
-                <nav>
-                    <ul>
-                        <li><a href="#">nav ul li a</a></li>
-                        <li><a href="#">nav ul li a</a></li>
-                        <li><a href="#">nav ul li a</a></li>
-                    </ul>
-                </nav>
-            </header>
+
+            <div class="container">
+
+                <header class="wrapper clearfix">
+                    <h1 class="title">h1.title</h1>
+                    <nav>
+                        <ul>
+                            <li><a href="#">nav ul li a</a></li>
+                            <li><a href="#">nav ul li a</a></li>
+                            <li><a href="#">nav ul li a</a></li>
+                        </ul>
+                    </nav>
+                </header>
+
+            </div>
+
         </div>
 
         <div class="slide">
