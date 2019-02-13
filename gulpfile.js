dee244fd71cd7f6d14a89d508d439e5c229c979a
Roni Laukkarinen <roni@laukkarinen.info>
2019-02-13 10:12:37 +0200

    Use cached to prevent loops, update helpers to stylefmt function

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ var scsslint    = require('gulp-scss-lint');
 var php2html    = require('gulp-php2html');
 var htmlmin     = require('gulp-htmlmin');
 var phpcs       = require('gulp-phpcs');
+var cache       = require('gulp-cached');
 
 /*
 
@@ -92,13 +93,19 @@ STYLES
 ======
 */
 
-var helpers = function( file ) {
+var stylefmtfile = function( file ) {
+
+    console.log(util.colors.white('[') + util.colors.yellow('Stylefmt') + util.colors.white('] ') + 'Automatically correcting file based on .stylelintrc...');
     var currentdirectory = process.cwd() + '/';
     var modifiedfile = file.path.replace( currentdirectory, '' );
     var filename = modifiedfile.replace(/^.*[\\\/]/, '')
     var correctdir = modifiedfile.replace( filename, '' );
 
     gulp.src(modifiedfile)
+
+        // Cache this action to prevent watch loop
+        .pipe(cache('stylefmtrunning'))
+
         // Run current file through stylefmt
         .pipe(stylefmt({ configFile: '.stylelintrc' }))
 
@@ -276,7 +283,7 @@ gulp.task('js-watch', ['js'], browsersync.reload);
 
 gulp.task('watch', ['browsersync'], function() {
 
-  gulp.watch(sassSrc, ['styles', 'scss-lint']).on( 'change', helpers );
+  gulp.watch(sassSrc, ['styles', 'scss-lint']).on( 'change', stylefmtfile );
   gulp.watch(jsSrc, ['js-watch']);
   gulp.watch(markupSrc, ['php']);
   gulp.watch(htmlSrc, ['html']);
